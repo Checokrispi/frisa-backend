80bf20843d2bf19e2f29fefa73ab65a49e301125
Checokrispi <105012973+Checokrispi@users.noreply.github.com>
2023-10-07 11:06:14 -0600

    Backend updates

--- server/src/db/sql-files/02-needyusers.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO NeedyUsers (email, password, fullName) VALUES
-('john.doe@domain.com', 'hashed_password123', 'John Doe'),
-('jane.smith@domain.com', 'hashed_password123', 'Jane Smith'),
-('alice.jones@domain.com', 'hashed_password123', 'Alice Jones'),
-('bob.brown@domain.com', 'hashed_password123', 'Bob Brown');
--- server/src/db/sql-files/04-oscs.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO OSCs (name, description, contactEmail, contactPhone, category) VALUES
-('OSC One', 'This is a description for OSC One.', 'osc1@domain.com', '123-456-7890', 'Category A'),
-('OSC Two', 'This is a description for OSC Two.', 'osc2@domain.com', '123-456-7891', 'Category B');
